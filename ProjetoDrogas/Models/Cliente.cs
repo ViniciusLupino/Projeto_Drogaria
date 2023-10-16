381850b95b5a2567ffab42265027555d2f5d8efa
Vinícius de Mattos Lupino <viniciusdemattoslupino@gmail.com>
2023-10-16 20:50:54 -0300

    Inicial

---
+++ ProjetoDrogas/Models/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoDrogas.Models
+{
+    public class Cliente
+    {
+        public Guid ClienteId { get; set; }
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+        public string Telefone { get; set; }
+        public string Cpf { get; set; }
+        public string Email { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Nascimento { get; set; }
+
+    }
+}
---
+++ ProjetoDrogas/Models/ItemVenda.cs
@@ -0,0 +1,37 @@
+﻿namespace ProjetoDrogas.Models
+{
+    public class ItemVenda
+    {
+        public Guid ItemVendaId { get; set; }
+
+        /**********************/
+        public Guid VendaId { get; set; }
+        public Venda? Vendas { get; set; }
+
+        /**********************/
+        public Guid ProdutoId { get; set; }
+        public Produto? Produtos { get; set; }
+
+        /**********************/
+
+        public int Quantidade { get; set; }
+        public string Preco { get; set; }
+
+        /**********************/
+
+        public decimal PrecoTotal
+        {
+            get
+            { 
+                //return Quantidade * int.Parse(Preco);
+                return Quantidade * decimal.Parse(Preco);
+            }
+            set { 
+            }
+        }
+
+
+
+
+    }
+}
